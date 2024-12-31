78e89cef37382271e8d86060e3a91ba01970854b
VictorSantos4 <victorevangelista153@gmail.com>
2024-12-31 05:01:29 -0300

    added new information from README file on repository

--- Declaração de variáveis/README.md
+++
@@ -1,3 +0,0 @@
-# Estudos Javascript
-
-### Reposítório criado com o intuito de obter uma visualização clara e objetiva dos respectivos estudos sobre JavaScript
--- Declaração de variáveis/main.js
+++ Declaração de variáveis/main.js
@@ -3,16 +3,16 @@
 // "Var" - Variável de escopo global, ou seja, funciona em qualquer parte do programa independente do lugar de declaração!
 
 if(true){
-    var a = 10
+    var a = 10;
+    a = 20
 }
-
 console.log(a) // Nota-se que o "console.log" está fora das chaves e mesmo assim retornará um resultado.
 
 
 // "Let" - Funciona apenas no bloco em que foi declarado!
 
 if(true){
-    let a = 20
+    let a = 20;
     console.log(a); // Agora com o uso do "let" o "console.log" só funcionará caso ele esteja dentro das chaves!
 }
 
@@ -25,7 +25,7 @@ if(true){
 
 if(true){
     const a = {nome: "victor"}; // Declaração da variável "a" que recebe um objeto com valor.
-    a.nome = "lucas" // Modificação do valor do objeto
+    a.nome = "lucas"; // Modificação do valor do objeto
 
     const b = {sobrenome: "silva"}; // Declaração da variável "a" que recebe um objeto com valor.
     b.sobrenome = "santos"; // Modificação do valor do objeto
